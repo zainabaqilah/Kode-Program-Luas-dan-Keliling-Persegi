891a82a8bf87373af458ce2ce0e428ab201c3fe5
zainabaqilah <112572182+zainabaqilah@users.noreply.github.com>
2022-09-10 11:20:23 +0700

    Add files via upload

---
+++ Kode Program Luas Dan Keliling Persegi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	cout<<"MEMBUAT PROGRAM UNTUK MENGHITUNG LUAS DAN KELILING"<<endl;
+	
+	int s,luas, keliling;
+	cout<<"sisi :";
+	cin>>s;
+	
+	luas=s*s;
+	cout<<"luas persegi :"<<luas<<endl;
+	keliling=4*s;
+	cout<<"keliling persegi :"<<keliling;
+	return 0;
+}
